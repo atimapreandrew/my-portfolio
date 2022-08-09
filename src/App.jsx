976d1d1b50c1ca7bae1c2fa5b85f418058c8c4a3
Andrew Atimapre <atimaxandrew@gmail.com>
2022-08-09 13:26:54 +0100

    My portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-black text-center text-4xl font-josefin">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      blue: "#",
+    },
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+      comfortaa: ["Comfortaa", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
